70ede6bb0bb5903cd0383d4c4057de8d7638e6b0
Ashvin Bondada <ashvinsaibondada@gmail.com>
2026-05-12 11:47:47 -0700

    Initial commit: HTML Auto Preview extension

---
+++ src/extension.ts
@@ -0,0 +1,45 @@
+import * as vscode from 'vscode';
+
+let lastOpenedUri = '';
+
+export function activate(context: vscode.ExtensionContext) {
+  const listener = vscode.workspace.onDidOpenTextDocument((doc) => {
+    if (doc.uri.scheme !== 'file') {
+      return;
+    }
+    const isHtml =
+      doc.languageId === 'html' || doc.fileName.endsWith('.html');
+    if (!isHtml) {
+      return;
+    }
+    const uriString = doc.uri.toString();
+    if (uriString === lastOpenedUri) {
+      return;
+    }
+    lastOpenedUri = uriString;
+
+    const openEditor = vscode.workspace
+      .getConfiguration('htmlAutoPreview')
+      .get<boolean>('openEditor', false);
+
+    vscode.commands.executeCommand('simpleBrowser.show', uriString).then(() => {
+      if (!openEditor) {
+        // Close the editor tab that was just opened for this HTML file
+        vscode.window.tabGroups.all.forEach((group) => {
+          group.tabs.forEach((tab) => {
+            if (
+              tab.input instanceof vscode.TabInputText &&
+              tab.input.uri.toString() === uriString
+            ) {
+              vscode.window.tabGroups.close(tab);
+            }
+          });
+        });
+      }
+    });
+  });
+
+  context.subscriptions.push(listener);
+}
+
+export function deactivate() {}
